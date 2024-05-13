5a5ff5dfcc9f03f89fa1bdd951f4a87eadcaaca5
吴炽彪 <1923356658@qq.com>
2024-05-14 01:00:57 +0800

    feat:安装依赖，配置PrimeVue+Tailwindcss

--- cbiu-admin-ui/src/main.ts
+++ cbiu-admin-ui/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue, { PrimeVueConfiguration } from "primevue/config";
+// @ts-ignore
+import Lara from './presets/lara'
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.component('Button',Button)
+app.component('InputText',InputText)
+app.use(PrimeVue,{
+    unstyled: true,
+    pt:Lara
+} as PrimeVueConfiguration).mount('#app')
